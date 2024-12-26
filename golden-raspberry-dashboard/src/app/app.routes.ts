422127441f08588b498128f01a7284af945bab58
giovannicaprio <giovanni.caprio@yahoo.com.br>
2024-12-26 11:04:12 -0300

    ajuste da rota default

--- golden-raspberry-dashboard/src/app/app.routes.ts
+++ golden-raspberry-dashboard/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component'; 
 import { ListComponent } from './list/list.component'; 
 export const routes: Routes = [
-    { path: '', component: AppComponent }, // Default route
+    { path: '', component: DashboardComponent }, // Default route
     { path: 'dashboard', component: DashboardComponent }, // Route for the list view
     { path: 'list', component: ListComponent }, // Route for the list view
     { path: '**', redirectTo: '' } // Redirect any unknown routes to the default route
